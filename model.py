d5ce79bd4140b552e928b2680c714a5ca8c05946
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-11-28 23:54:04 +0100

    sequence in dense

--- model.py
+++ model.py
@@ -1,5 +1,5 @@
 from keras import Input, Model
-from keras.src.layers import LSTM, Dense, Concatenate
+from keras.src.layers import LSTM, Dense, Concatenate, Flatten
 from numpy import concatenate
 from tensorflow import keras
 import tensorflow as tf
@@ -40,25 +40,32 @@ def generateModel():
     global nChars
 
     # Define the first input
-    input_1 = Input(shape=(tokensBag))  # Replace additional_input_dim with the actual dimension of your second input
+    input_1 = Input(shape=(seqLen, tokensBag))  # Replace additional_input_dim with the actual dimension of your second input
+    flatten_1 = Flatten()(input_1)
 
     # Define the second input
-    input_2 = Input(shape=(nChars))
+    input_2 = Input(shape=(seqLen, nChars))
+    flatten_2 = Flatten()(input_2)
+
 
     # Define the first input branch
-    lstm_1 = Dense(tokensBag)(input_1)
+    lstm_1 = Dense(tokensBag*2)(flatten_1)
 
     # Define the second input branch
-    lstm_2 = Dense(tokensBag)(input_2)
+    lstm_2 = Dense(tokensBag*2)(flatten_2)
 
     # Concatenate the LSTM outputs
     merged_outputs = Concatenate()([lstm_1, lstm_2])
 
+    # Play
+    dense1 = Dense(tokensBag*2)(merged_outputs)
+    dense2 = Dense(tokensBag*2)(dense1)
+
     # Output 1: Dense layer for classification
-    output_1 = Dense(tokensBag, activation='softmax', name='output_1')(merged_outputs)
+    output_1 = Dense(tokensBag, activation='softmax', name='output_1')(dense2)
 
     # Output 2: Another Dense layer for regression
-    output_2 = Dense(nChars, activation='linear', name='output_2')(merged_outputs)
+    output_2 = Dense(nChars, activation='linear', name='output_2')(dense2)
 
     # Create the model
     model = Model(inputs=[input_1, input_2], outputs=[output_1, output_2])
@@ -133,6 +140,7 @@ def predictSeq():
     global curSeqChars
     global tokensBag
     global nChars
+    global seqLen
 
     x1 = curSeqBag[:]
     x2 = curSeqChars[:]
@@ -142,6 +150,11 @@ def predictSeq():
 
     x1 = np.array(x1)
     x2 = np.array(x2)
+
+    psb_shape = (1, seqLen, tokensBag)
+    psc_shape = (1, seqLen, nChars)
+    x1 = np.zeros(psb_shape)
+    x2 = np.zeros(psc_shape)
 
     return model.predict([x1, x2])
 
@@ -183,35 +196,26 @@ def fitSeq():
         psb = pad(psb, tokensBag)
         psc = pad(psc, nChars)
 
-        csb = curSeqBag[:]
-        csc = curSeqChars[:]
+        csb = curSeqBag[-1]
+        csc = curSeqChars[-1]
 
-        csb = pad(csb, tokensBag)
-        csc = pad(csc, nChars)
-
-        '''
-        psb = [psb]
-        psc = [psc]
-        csb = [csb]
-        csc = [csc]
-        '''
+        #csb = pad(csb, tokensBag)
+        #csc = pad(csc, nChars)
 
         psb = np.array(psb)
         psc = np.array(psc)
         csb = np.array(csb)
         csc = np.array(csc)
 
-        '''
-        psb_shape = (seqLen, tokensBag)
-        psc_shape = (seqLen, nChars)
+        psb_shape = (1, seqLen, tokensBag)
+        psc_shape = (1, seqLen, nChars)
         psb = np.zeros(psb_shape)
         psc = np.zeros(psc_shape)
 
-        csb_shape = (seqLen, tokensBag)
-        csc_shape = (seqLen, nChars)
+        csb_shape = (1, tokensBag)
+        csc_shape = (1, nChars)
         csb = np.zeros(csb_shape)
         csc = np.zeros(csc_shape)
-        '''
 
         print("psb shape: ", psb.shape)
         print("psc shape: ", psc.shape)
