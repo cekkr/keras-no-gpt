c0dd4ce53e4ea548e1efe52717fa8d7afd7269aa
Riccardo Cecchini <rcecchini.ds@gmail.com>
2023-11-28 22:20:23 +0100

    it works, but does it makes sense?

--- model.py
+++ model.py
@@ -14,7 +14,7 @@ import os
 
 from tensorflow.python.layers.base import Layer
 
-modelName = 'noSql.h5'
+modelName = 'noGpt.h5'
 
 """
 ## Build the model: a single LSTM layer
@@ -28,24 +28,28 @@ seqLen = 16 # Sequence length for LSTM layer
 tokensBag = 256
 
 epochsPerSeq = 10
-batchSize = 10
+batchSize = 1
 
 model = None
 
 def generateModel():
     global model
+    global seqLen
+    global batchSize
+    global tokensBag
+    global nChars
 
     # Define the first input
-    input_1 = Input(shape=(seqLen, tokensBag))  # Replace additional_input_dim with the actual dimension of your second input
+    input_1 = Input(shape=(tokensBag))  # Replace additional_input_dim with the actual dimension of your second input
 
     # Define the second input
-    input_2 = Input(shape=(seqLen, nChars))
+    input_2 = Input(shape=(nChars))
 
     # Define the first input branch
-    lstm_1 = LSTM(tokensBag)(input_1)
+    lstm_1 = Dense(tokensBag)(input_1)
 
     # Define the second input branch
-    lstm_2 = LSTM(tokensBag)(input_2)
+    lstm_2 = Dense(tokensBag)(input_2)
 
     # Concatenate the LSTM outputs
     merged_outputs = Concatenate()([lstm_1, lstm_2])
@@ -127,8 +131,19 @@ def pushChar(ch):
 def predictSeq():
     global curSeqBag
     global curSeqChars
+    global tokensBag
+    global nChars
 
-    return model.predict([curSeqBag, curSeqChars])
+    x1 = curSeqBag[:]
+    x2 = curSeqChars[:]
+
+    x1 = pad(x1, tokensBag)
+    x2 = pad(x2, nChars)
+
+    x1 = np.array(x1)
+    x2 = np.array(x2)
+
+    return model.predict([x1, x2])
 
 def print_callback(epoch, logs):
     global epochsPerSeq
@@ -168,33 +183,50 @@ def fitSeq():
         psb = pad(psb, tokensBag)
         psc = pad(psc, nChars)
 
-        #csb = [curSeqBag[-1]]
-        #csc = [curSeqChars[-1]]
-
         csb = curSeqBag[:]
         csc = curSeqChars[:]
 
         csb = pad(csb, tokensBag)
         csc = pad(csc, nChars)
 
-        psb = np.array([psb])
-        psc = np.array([psc])
-        csb = np.array([csb])
-        csc = np.array([csc])
-
         '''
-        print("prevSeqBag dim: ", psb.ndim)
-        print("prevSeqChars dim: ", psc.ndim)
-        print("curSeqBag dim: ", csb.ndim)
-        print("curSeqChars dim: ", csc.ndim)
+        psb = [psb]
+        psc = [psc]
+        csb = [csb]
+        csc = [csc]
         '''
 
-        model.fit([psb, psc], [csb, csc], epochs=epochsPerSeq, batch_size=batchSize, callbacks=[LambdaCallback(on_epoch_end=print_callback)])
+        psb = np.array(psb)
+        psc = np.array(psc)
+        csb = np.array(csb)
+        csc = np.array(csc)
+
+        '''
+        psb_shape = (seqLen, tokensBag)
+        psc_shape = (seqLen, nChars)
+        psb = np.zeros(psb_shape)
+        psc = np.zeros(psc_shape)
+
+        csb_shape = (seqLen, tokensBag)
+        csc_shape = (seqLen, nChars)
+        csb = np.zeros(csb_shape)
+        csc = np.zeros(csc_shape)
+        '''
+
+        print("psb shape: ", psb.shape)
+        print("psc shape: ", psc.shape)
+        print("csb shape: ", csb.shape)
+        print("csc shape: ", csc.shape)
+
+        input = [psb, psc]
+        output = [csb, csc]
+
+        model.fit(input, output, epochs=epochsPerSeq, batch_size=batchSize, callbacks=[LambdaCallback(on_epoch_end=print_callback)])
 
         model.save(modelName)
 
         res = predictSeq()
-        prevBag = res[0]
+        prevBag = res[0][-1]
 
 # Default
 initBag()
